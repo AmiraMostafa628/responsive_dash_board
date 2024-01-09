881fc12c7a84835047c249ca3b4d13751791149d
amira <amira.mostafa123455@gmail.com>
2024-01-09 18:22:24 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ResponsiveDashBoard());
+}
+
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
+
