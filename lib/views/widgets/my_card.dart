d6e7645a913b6ac93b693ba5f2fc652ddac28533
amira <amira.mostafa123455@gmail.com>
2024-02-04 19:56:52 +0200

    app finish

--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -11,56 +11,59 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
         aspectRatio: 420/215,
-      child: Container(
-        decoration: ShapeDecoration(
-          image: const DecorationImage(
-            fit: BoxFit.fill,
-              image: AssetImage(Images.cardBackground)),
-          color: const Color(0xFF4EB7F2),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Container(
+          decoration: ShapeDecoration(
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+                image: AssetImage(Images.cardBackground)),
+            color: const Color(0xFF4EB7F2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              contentPadding: const EdgeInsets.only(left: 31,right: 42,top: 16),
-              title: Text('Name Card',
-                style: AppStyles.styleRegular16(context).copyWith(
-                  color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.only(left: 31,right: 42,top: 16),
+                title: Text('Name Card',
+                  style: AppStyles.styleRegular16(context).copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+                subtitle: Text('Syah Bandi',
+                  style: AppStyles.styleMedium20(context),
+                ),
+                trailing: SvgPicture.asset(Images.gallery),
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right:24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('0918 8124 0042 8129',
+                      style: AppStyles.styleSemiBold24(context).copyWith(
+                        color: Colors.white
+                      ),
+                    ),
+                    Text('12/20 - 124',
+                      style: AppStyles.styleRegular16(context).copyWith(
+                          color: Colors.white
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              subtitle: Text('Syah Bandi',
-                style: AppStyles.styleMedium20(context),
-              ),
-              trailing: SvgPicture.asset(Images.gallery),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(right:24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text('0918 8124 0042 8129',
-                    style: AppStyles.styleSemiBold24(context).copyWith(
-                      color: Colors.white
-                    ),
-                  ),
-                  Text('12/20 - 124',
-                    style: AppStyles.styleRegular16(context).copyWith(
-                        color: Colors.white
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Flexible(
-              child: SizedBox(
-                height: 26,
-              ),
-            )
-          ],
+              const Flexible(
+                child: SizedBox(
+                  height: 26,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
